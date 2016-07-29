80f19cd054ab175c54e95af810a47296e24d11fa
Andres Gomez Urquiza <andres@fractal.ai>
2016-07-28 23:40:57 -0500

    save

--- src/digitalize/core.clj
+++ src/digitalize/core.clj
@@ -43,10 +43,8 @@
 
 (def chars-to-dash
   "List of chars to convert to dashes"
-  (assoc (zipmap (map str "!@$%^&*()_+?/-")
-                 (repeat "-"))
-         " " "-"
-         "#" "-"))
+  (zipmap (map str "!@$%^&*()_+?/- #,")
+                 (repeat "-")))
 
 (def standard-keys
   "Lat long names in spanish"
@@ -117,29 +115,30 @@
   [o & {:keys [clean-keys clean-numbers]
         :or {clean-keys true,
              clean-numbers true}}]
-  (let [data (case (category o)
-               :coll (if-not (empty? o)
-                         (map #(digitalize % :clean-keys clean-keys
-                                             :clean-numbers clean-numbers)
-                              o))
-               :map  (let [ks (keys o)
-                           vs (vals o)
-                           new-ks (if clean-keys
-                                      (map standard-keyword ks)
-                                      ks) ;dont touch keys ks
-                           new-vs (map #(digitalize % :clean-keys clean-keys
-                                                      :clean-numbers clean-numbers)
-                                       vs)]
-                   (zipmap new-ks new-vs))
-               :string (let [s (s/trim o)]
-                         (cond
-                           (snumber? s) (if clean-numbers
-                                            (str->number s)
-                                            s)
-                           (empty? s) nil
-                          :else s))
-               :number (if clean-numbers
-                         (simplify-int o)
-                         o)
-               o)]
-    (remove-nils data)))
+  (try (let [data (case (category o)
+                    :coll (if-not (empty? o)
+                            (map #(digitalize % :clean-keys clean-keys
+                                              :clean-numbers clean-numbers)
+                                 o))
+                    :map  (let [ks (keys o)
+                                vs (vals o)
+                                new-ks (if clean-keys
+                                         (map standard-keyword ks)
+                                         ks) ;dont touch keys ks
+                                new-vs (map #(digitalize % :clean-keys clean-keys
+                                                         :clean-numbers clean-numbers)
+                                            vs)]
+                            (zipmap new-ks new-vs))
+                    :string (let [s (s/trim o)]
+                              (cond
+                                (snumber? s) (if clean-numbers
+                                               (str->number s)
+                                               s)
+                                (empty? s) nil
+                                :else s))
+                    :number (if clean-numbers
+                              (simplify-int o)
+                              o)
+                    o)]
+         (remove-nils data))
+       (catch Exception e o)))
